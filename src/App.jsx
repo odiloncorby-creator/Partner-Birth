2e3ba84c638627efd74f6b96bb5e3a5bd1d941ac
odiloncorby-creator <odilon.corby@gmail.com>
2026-04-30 21:06:52 +0200

    feat: wire EnDirect view from Calendrier activation button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Bagages from './pages/Bagages'
 import ModeJ from './pages/ModeJ'
 import Info from './pages/Info'
 import Calendrier from './pages/Calendrier'
+import EnDirect from './pages/EnDirect'
 
 const PAGES = {
   prepare: Prepare,
@@ -17,10 +18,15 @@ const PAGES = {
 export default function App() {
   const [activeTab, setActiveTab] = useState('prepare')
   const [selectedTipId, setSelectedTipId] = useState(null)
+  const [enDirectActive, setEnDirectActive] = useState(false)
 
   const handleTabChange = (tab) => {
     setActiveTab(tab)
     setSelectedTipId(null)
+  }
+
+  if (enDirectActive) {
+    return <EnDirect onBack={() => setEnDirectActive(false)} />
   }
 
   const ActivePage = PAGES[activeTab]
@@ -32,6 +38,7 @@ export default function App() {
           selectedTipId={selectedTipId}
           onSelectTip={setSelectedTipId}
           onBack={() => setSelectedTipId(null)}
+          onStartEnDirect={() => setEnDirectActive(true)}
         />
       </main>
       <BottomNav activeTab={activeTab} onTabChange={handleTabChange} />
